36f0c0c4de015c555ee3f7eacd2f7edc3bdf8e17
ldjontz <ldjontz@crimson.ua.edu>
2023-10-02 12:10:39 -0500

    update Homework1

--- Computer Science/CS201/Homework1/Homework1.cpp
+++ Computer Science/CS201/Homework1/Homework1.cpp
@@ -1,9 +1,9 @@
 /*Name: Luca Jontz
     Email: ldjontz@crimson.ua.edu
     Course Section: Fall 2023 CS 201
-    Homework 0
-    Instructions to Compile: g++ Homework1.cpp
-    Instructions to execute: ./a.exe 
+    Homework 1
+    To Compile: g++ Homework1.cpp
+    To execute: ./a.exe 
 */
 #include <iostream>
 #include <iterator>
@@ -13,7 +13,11 @@ using namespace std;
 
 template <class RandomIt>
 constexpr void heapsort(RandomIt first, RandomIt last){
-
+    /*BuildMaxHeap(first, last)
+    for i = last downto 1
+        exchange first[0] with first[last]
+        first.heap-size = first.heap-size - 1
+        MaxHeapify(A, 0)*/
 }
 
 template <class RandomIt>
