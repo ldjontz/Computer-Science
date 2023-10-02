29d185862f5563374b13ebb518ffc77ea265d699
ldjontz <ldjontz@crimson.ua.edu>
2023-10-02 16:20:13 -0500

    update Homework1

--- Computer Science/CS201/Homework1/Homework1.cpp
+++ Computer Science/CS201/Homework1/Homework1.cpp
@@ -11,13 +11,38 @@
 
 using namespace std;
 
+/*MaxHeapify(A, i){
+    l=left(i)
+    r=right(i)
+    if l <= A.heap-size && A[l] > A[i]{
+        largest = l
+    }
+    else largest = i
+    if r <= A.heap-size && A[r] > A[largest]{
+        largest = r
+    }
+    if largest != i{
+        exchange A[i] with A[largest]
+        MaxHeapify(A, largest)
+    }
+}*/
+
+/*BuildMaxHeap(A, n){
+    A.heap-size = n
+    for i = n/2 downto 0{
+        MaxHeapify(A, i)
+    }
+}*/
+
+
+
 template <class RandomIt>
 constexpr void heapsort(RandomIt first, RandomIt last){
     /*BuildMaxHeap(first, last)
     for i = last downto 1
         exchange first[0] with first[last]
         first.heap-size = first.heap-size - 1
-        MaxHeapify(A, 0)*/
+        MaxHeapify(first, 0)*/
 }
 
 template <class RandomIt>
